2a364050b146cca1fe0c22411cb77aefc8be6beb
Nico Krause <inspiraluna@gmail.com>
2017-11-06 11:27:01 +0100

    modified start command

--- start-docker.sh
+++ start-docker.sh
@@ -1,4 +1,4 @@
-docker run -d -e ROOT_URL=http://www.nicokrause.com -e MONGO_URL=mongodb://nicokrause_com -e MONGO_OPLOG_URL=mongodb://nicokrause.com/nicokrause_com_oplog -v /var/www/meteor/nicokrause.com:/bundle -p 3002:80 --log-driver=syslog meteorhacks/meteord:base
+#old bundle config
+#docker run -d -e ROOT_URL=http://www.nicokrause.com -e MONGO_URL=mongodb://nicokrause_com -e MONGO_OPLOG_URL=mongodb://nicokrause.com/nicokrause_com_oplog -v /var/www/meteor/nicokrause.com:/bundle -p 3002:80 --log-driver=syslog meteorhacks/meteord:base
 
-
-docker run -d -e ROOT_URL=http://www.nicokrause.com -e MONGO_URL=mongodb://nicokrause_com:XXXXX@nicokrause.com/nicokrause_com -v /var/www/meteor/nicokrause.com:/bundle -p 3002:3000 --log-driver=syslog  profilepage_meteor
+sudo docker run -d -e PWD=/var/www/meteor/nicokrause.com/profilePage -e MONGO_URL=mongodb://nicokrause_com:omnamahshivaya@nicokrause.com/nicokrause_com -p 3002:3000 --log-driver=syslog  profilepage_meteor
