4fdeb036e8621034a18294acfeee40702c5a3d99
Nico Krause <nico@le-space.de>
2017-11-06 15:27:44 +0500

    modified docker config

--- build-docker.sh
+++ build-docker.sh
@@ -1,3 +1,8 @@
 #docker build -t NicoKrause.com/app .
-meteor build ../build/ --architecture os.linux.x86_64
-scp build/profilePage.tar.gz nico@le-space.de:/var/www/meteor/nicokrause.com/
+
+#meteor build over bundle
+#meteor build ../build/ --architecture os.linux.x86_64
+#scp build/profilePage.tar.gz nico@le-space.de:/var/www/meteor/nicokrause.com/
+
+#meteor build via docker-compose 
+sudo -- sh -c 'export PWD=/var/www/meteor/nicokrause.com/profilePage; docker-compose build'
